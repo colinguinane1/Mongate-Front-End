9d35f2383a5b180d84f4d169edd57e792819d9f2
colinguinane1 <colinguinane@gmail.com>
2024-12-29 15:21:44 -0700

    feat: enhance home page layout with tech stack display and adjust hero section styles

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,29 @@ import CAnimatedButton from "@/components/ui/c-animated-button";
 import CAnimatedButtonOutline from "@/components/ui/c-animated-button-outline";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
+import { FaNodeJs } from "react-icons/fa";
 
 export default function Home() {
+  const techStack = [
+    {
+      name: "Node.JS",
+      icon: <FaNodeJs color="green" className="w-16 h-16" />,
+      description: "Javascript for the web.",
+    },
+  ];
+
   return (
     <div className="grid place-content-center h-[40vh] ">
       <HeroSection />
-      <div></div>
+      <div className="mt-20 p-4">
+        {techStack.map((tech) => (
+          <div className="bg-primary/10 w-52 h-52 flex flex-col items-center justify-center rounded-lg">
+            {tech.icon}
+            <h1 className="font-bold  text-2xl">{tech.name}</h1>
+            <p>{tech.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
