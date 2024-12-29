15e87ea8ddb7655ef8fc47ba3590adea59df396c
Colin Guinane <colinguinane@gmail.com>
2024-12-29 16:50:40 -0700

    Update: z index of header

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   const { user } = useUser();
   return (
     <header className="flex items-center justify-center fixed p-4 w-screen">
-      <div className=" backdrop-blur-3xl p-2 px-4 flex w-full justify-between motion-preset-blur-down-lg items-center gap-4 rounded-md border">
+      <div className=" backdrop-blur-3xl z-50 p-2 px-4 flex w-full justify-between motion-preset-blur-down-lg items-center gap-4 rounded-md border">
         <Link className="text-gray-400" href="/">
           Home
         </Link>
