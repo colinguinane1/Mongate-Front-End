94e36a45ab31f3a13422dcbe5eeeddb1ac59b166
colinguinane1 <colinguinane@gmail.com>
2025-01-02 15:29:45 -0700

    refactor: remove unused MDX components and optimize date formatting in getDocs function

--- lib/gett-docs.ts
+++ lib/gett-docs.ts
@@ -54,15 +54,15 @@ export async function getDocs(): Promise<Doc[]> {
 }
 
 export function formatDate(date: string, includeRelative = false) {
-  let currentDate = new Date();
+  const currentDate = new Date();
   if (!date.includes("T")) {
     date = `${date}T00:00:00`;
   }
-  let targetDate = new Date(date);
+  const targetDate = new Date(date);
 
-  let yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
-  let monthsAgo = currentDate.getMonth() - targetDate.getMonth();
-  let daysAgo = currentDate.getDate() - targetDate.getDate();
+  const yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
+  const monthsAgo = currentDate.getMonth() - targetDate.getMonth();
+  const daysAgo = currentDate.getDate() - targetDate.getDate();
 
   let formattedDate = "";
 
@@ -76,7 +76,7 @@ export function formatDate(date: string, includeRelative = false) {
     formattedDate = "Today";
   }
 
-  let fullDate = targetDate.toLocaleString("en-us", {
+  const fullDate = targetDate.toLocaleString("en-us", {
     month: "short",
     day: "numeric",
     year: "numeric",
