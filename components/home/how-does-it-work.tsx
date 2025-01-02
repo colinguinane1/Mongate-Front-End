0ed687546ed8f7be85290452763d0c3ad7094650
colinguinane1 <colinguinane@gmail.com>
2025-01-02 07:55:25 -0700

    feat: enhance "How Does It Work" section; restructure content into an ordered list and add environment variable setup instructions

--- components/home/how-does-it-work.tsx
+++ components/home/how-does-it-work.tsx
@@ -13,27 +13,53 @@ export default function HowDoesItWork() {
     <div className="flex flex-col w-screen items-center justify-center p-4 pb-20 space-y-4">
       <div className="flex flex-col gap-4  max-w-6xl">
         <p className="font-bold text-4xl">Getting Started</p>
-        <div className="flex flex-col items-center justify-center gap-4">
-          <div className="flex items-center gap-4">
-            <h1 className="bg-foreground w-8 h-8 text-background font-extrabold text-2xl inline-flex items-center justify-center rounded-full">
-              1.
-            </h1>
-            <p>Clone the repositories.</p>
-          </div>
-          <ScriptCopyBtn
-            className="w-60"
-            codeLanguage="shell"
-            lightTheme="nord"
-            darkTheme="vitesse-dark"
-            commandMap={frontend}
-          ></ScriptCopyBtn>
-          <ScriptCopyBtn
-            className="w-60"
-            codeLanguage="shell"
-            lightTheme="nord"
-            darkTheme="vitesse-dark"
-            commandMap={backend}
-          ></ScriptCopyBtn>
+        <div className="flex flex-col  gap-4">
+          <ol className="">
+            <li>
+              <div className="flex items-center gap-4">
+                <h1 className="bg-foreground w-8 h-8 text-background font-extrabold text-2xl inline-flex items-center justify-center rounded-full">
+                  1.
+                </h1>
+                <p>Clone the repositories.</p>
+              </div>
+              <ScriptCopyBtn
+                className="w-60"
+                codeLanguage="shell"
+                lightTheme="nord"
+                darkTheme="vitesse-dark"
+                commandMap={frontend}
+              ></ScriptCopyBtn>
+              <ScriptCopyBtn
+                className="w-60"
+                codeLanguage="shell"
+                lightTheme="nord"
+                darkTheme="vitesse-dark"
+                commandMap={backend}
+              ></ScriptCopyBtn>
+            </li>
+            <li>
+              <div className="flex items-center gap-4">
+                <h1 className="bg-foreground w-8 h-8 text-background font-extrabold text-2xl inline-flex items-center justify-center rounded-full">
+                  2.
+                </h1>
+                <p>Set up your Enviornment variables.</p>
+              </div>
+              <ScriptCopyBtn
+                className="w-60"
+                codeLanguage="shell"
+                lightTheme="nord"
+                darkTheme="vitesse-dark"
+                commandMap={frontend}
+              ></ScriptCopyBtn>
+              <ScriptCopyBtn
+                className="w-60"
+                codeLanguage="shell"
+                lightTheme="nord"
+                darkTheme="vitesse-dark"
+                commandMap={backend}
+              ></ScriptCopyBtn>
+            </li>
+          </ol>{" "}
         </div>
       </div>
     </div>
