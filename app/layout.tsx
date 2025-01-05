0f36cd5500fd474f4c6e330ef2b8941c17ea3231
Colin Guinane <colinguinane@gmail.com>
2025-01-05 10:08:19 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,50 +12,9 @@ import { siteConfig } from "@/config/site";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  metadataBase: new URL(siteConfig.url),
-  description: siteConfig.description,
-  keywords: ["Next.js", "React", "Express.JS", "MongoDB", "Node.JS"],
-  authors: [
-    {
-      name: "Colin Guinane",
-      url: "https://c-g.dev",
-    },
-  ],
-  creator: "colinguinane1",
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-    images: [
-      {
-        url: siteConfig.ogImage,
-        width: 1200,
-        height: 630,
-        alt: siteConfig.name,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [siteConfig.ogImage],
-    creator: "@colinguinane1",
-  },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-  manifest: `${siteConfig.url}/site.webmanifest`,
-};
+  title: "Mongate - The MERN Stack Template",
+  description: "Template for the popular MERN stack",
+}
 
 // Import the Krub font with the desired settings
 const krub = Krub({
