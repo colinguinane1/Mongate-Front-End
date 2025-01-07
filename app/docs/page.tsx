1332cc52ee11363dd193e82a488f408c989cc80f
colinguinane1 <colinguinane@gmail.com>
2025-01-07 14:28:11 -0700

    Refactor DocsListPage to redirect to the getting started page; remove unused imports and components

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,46 +1,7 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import { getDocs } from "@/lib/gett-docs";
-import { SidebarTrigger } from "@/components/ui/sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import Link from "next/link";
+"use client";
+import { useRouter } from "next/router";
 
 export default async function DocsListPage() {
-  const docs = await getDocs();
-  return (
-    <section className="mt-20 w-screen p-4">
-      <div className="px-4 max-w-2xl md:hidden flex items-center gap-4 w-full">
-        <AppSidebar docs={docs} />
-        
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/">Home</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/docs">Documentation</BreadcrumbLink>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
-      </div>
-      <div className="flex flex-col gap-4 p-4">
-        {docs.map((doc, idx) => (
-          <Link
-            key={doc.slug}
-            href={`/docs/${doc.slug}`}
-            className={`border p-2 rounded-lg  hover:bg-primary/10 w-full  transition-all`}
-          >
-            {idx + 1}. {doc.metadata.title}
-          </Link>
-        ))}
-      </div>
-    </section>
-  );
+  const router = useRouter();
+  return router.push("/docs/getting-started");
 }
