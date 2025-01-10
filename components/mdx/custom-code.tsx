af3862344e02eaee13a2e98cc1f1e42e52795ccf
Colin Guinane <colinguinane@gmail.com>
2025-01-10 15:37:55 -0700

    Update custom-code.tsx

--- components/mdx/custom-code.tsx
+++ components/mdx/custom-code.tsx
@@ -20,39 +20,39 @@ const Code = (props: any, className: any) => {
   };
 
   return (
-    <div
-      className={cn(
-        "relative rounded-md   dark:bg-card/20 bg-foreground/95 p-2 overflow-y-auto     max-h-[350px]",
-        className
-      )}
-    >
-      {/* Code block with copy button */}
-      <div className="flex fixed right-2 top-2 right-2 justify-between items-center">
-        <button
-          className="relative w-8 h-8 border bg-transparent backdrop-blur-lg flex items-center justify-center  rounded-md"
-          onClick={handleCopy}
-          aria-label={copied ? "Copied" : "Copy to clipboard"}
-        >
-          <span className="sr-only">{copied ? "Copied" : "Copy"}</span>
-          <Clipboard
-            color={"white"}
-            className={`h-4 w-7 transition-all duration-300 ${
-              copied ? "scale-0" : "scale-100"
-            }`}
-          />
-          <Check
-            className={`absolute inset-0 text-primary m-auto h-4 w-4 transition-all duration-300 ${
-              copied ? "scale-100" : "scale-0"
-            }`}
-          />
-        </button>
-      </div>
-
-      <code ref={codeRef} className="whitespace-pre 2">
-        {props.children}
-      </code>
+  <div
+    className={cn(
+      "relative rounded-md dark:bg-card/20 bg-foreground/95 p-2 overflow-y-auto max-h-[350px]",
+      className
+    )}
+  >
+    {/* Code block with copy button */}
+    <div className="sticky top-2 right-2 flex justify-between items-center">
+      <button
+        className="relative w-8 h-8 border bg-transparent backdrop-blur-lg flex items-center justify-center rounded-md"
+        onClick={handleCopy}
+        aria-label={copied ? "Copied" : "Copy to clipboard"}
+      >
+        <span className="sr-only">{copied ? "Copied" : "Copy"}</span>
+        <Clipboard
+          color={"white"}
+          className={`h-4 w-7 transition-all duration-300 ${
+            copied ? "scale-0" : "scale-100"
+          }`}
+        />
+        <Check
+          className={`absolute inset-0 text-primary m-auto h-4 w-4 transition-all duration-300 ${
+            copied ? "scale-100" : "scale-0"
+          }`}
+        />
+      </button>
     </div>
-  );
+
+    <code ref={codeRef} className="whitespace-pre">
+      {props.children}
+    </code>
+  </div>
+);
 };
 
 export default Code;
